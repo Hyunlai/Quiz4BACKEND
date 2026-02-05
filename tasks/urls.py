af9e53dd63855e288a70f163bbb4d121c93b1d0a
Carlos Nathaniel <Carlosnathaniel20@gmail.com>
2026-02-05 22:43:14 +0800

    Added more Urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register(r'', views.TaskViewSet, basename='task')
+
 urlpatterns = [
-    path('', views.TaskListView, name='task-list'),
-    path('<int:pk>/', views.TaskDetailView, name='task-detail'),
-    path('create/', views.TaskCreateView, name='task-create'),
+    path('', include(router.urls)),
 ]
