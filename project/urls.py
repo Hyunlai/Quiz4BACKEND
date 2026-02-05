af9b9bcd40b109fb000868fdd220a44aba4d1fff
Carlos Nathaniel <Carlosnathaniel20@gmail.com>
2026-02-05 22:41:38 +0800

    Added Urls for the router

--- project/urls.py
+++ project/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register(r'', views.ProjectViewSet, basename='project')
+
 urlpatterns = [
-    path('', views.ProjectListView, name='project-list'),
-    path('<int:pk>/', views.ProjectDetailView, name='project-detail'),
-    path('create/', views.ProjectCreateView, name='project-create'),
+    path('', include(router.urls)),
 ]
 
